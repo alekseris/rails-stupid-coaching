c0693b2fd12e312be8fb82ff7987484a6d0b7eba
Alex R <jpalre@outlook.com>
2019-05-20 14:47:00 +0900

    it works til optional

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get   'ask', to: 'questions#ask', as: :ask
+  get   'answer', to: 'questions#answer', as: :answer
+  # post  'answer', to: 'questions#create', as: :answer
+  # get   'questions/:id', to: 'questions#show', as: :question
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
